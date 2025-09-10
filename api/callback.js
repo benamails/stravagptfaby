f104757de4b2eb7fca120871aa52c3ccd04c4a13
benamails <benamails+github@gmail.com>
2025-09-10 15:56:56 +0200

    On casse tout et on recommence

--- api/callback.js
+++
@@ -1,5 +0,0 @@
-// api/callback.js
-import oauthCb from './oauth/callback.js'
-export default async function handler(req, res) {
-  return oauthCb(req, res)
-}
--- api/ping.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  console.log("HIT /api/ping", { method: req.method, ua: req.headers['user-agent'] });
-  res.json({ ok: true, ts: Date.now() });
-}
